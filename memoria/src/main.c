8ef2a32ff8212b431bcd3254e3b78df810b96760
Juan manuel iglesias arregui <129231361+JuanmaIglesias10@users.noreply.github.com>
2024-04-16 16:06:12 +0000

    Modifico comentario

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,7 +1,7 @@
 #include <../include/main.h>
 
 int main(void) {
-    // ------------ MEMORIA SERVER DE CPU -------------
+    // ------------ MEMORIA SERVER DE CPU-KERNEL-ENTRADASALIDA -------------
 
 	t_log* logger = iniciar_logger("logMemoria.log", "Server", LOG_LEVEL_DEBUG);
 	server_escuchar(logger, "8003");
