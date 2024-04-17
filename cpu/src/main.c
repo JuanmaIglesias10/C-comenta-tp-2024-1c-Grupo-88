874ca8d93338904ccd374140abd520327bbd4c7d
JuanmaIglesias10 <juanmaiglesiascasla@gmail.com>
2024-04-17 00:59:43 +0000

    Estandarizo comentarios de las conexiones

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -4,14 +4,14 @@ int main(void) {
 
 	t_log *logger;
 
-	// ------------  CPU CLIENTE DE MEMORIA -------------------
+	// ------------  CPU ------> MEMORIA -------------------
 	
 	t_config* config = iniciar_config("../cpu.config");
 	logger = iniciar_logger("logCPU.log","Client",LOG_LEVEL_INFO);
 
 	conectarse(config, "IP", "PUERTO_MEMORIA", "cpu", logger);
 
-	// ----------- CPU SERVER DE KERNEL ------------------------
+	// ----------- KERNEL ------> CPU ------------------------
 
 	logger = iniciar_logger("logCPU.log", "Server", LOG_LEVEL_DEBUG);
 	server_escuchar(logger, "4445");
