ae84d45737ef2551e92771952c73f0f4d4a564c7
JuanmaIglesias10 <juanmaiglesiascasla@gmail.com>
2024-06-09 21:28:44 +0000

    creo mmu.c/.h para organizar mejor

---
+++ cpu/src/mmu.h
@@ -0,0 +1,9 @@
+#include "utils.h"
+#include "math.h"
+#include "diccionario_cpu.h"
+
+int obtener_nro_pagina(int direccionLogica);
+
+int obtener_desplazamiento_pagina(int direccionLogica);
+
+uint32_t calcular_direccion_fisica(int direccion_logica, t_cde* cde);
