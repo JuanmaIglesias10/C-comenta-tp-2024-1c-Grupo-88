a1622e39ff686c78c42a9a7b191b68f5082d9fd0
GabNavarro01 <ganavarro@frba.utn.edu.ar>
2024-06-13 20:19:03 +0000

    Elimino Logs Innecesarios

--- entradasalida/src/atender_IO.c
+++ entradasalida/src/atender_IO.c
@@ -23,19 +23,14 @@ void atender_kernel()
 {
      while (1) {
              mensajeKernelIO cod_op = recibir_codOp(fd_kernel);
-                    log_info(logger_IO,"%d",cod_op);
-                    log_info(logger_IO,"xd");
              switch (cod_op) {
                  case SLEEP:
                     //  exec_IO_Generica();
-                     log_info(logger_IO,"1");
                     t_buffer* buffer_recibido = recibir_buffer(fd_kernel);
                     uint8_t unidadesDeTiempo = leer_buffer_uint8(buffer_recibido);
                     usleep(unidadesDeTiempo * 1000);
-                    log_info(logger_IO,"2");
                     destruir_buffer(buffer_recibido);
                     enviar_codOp(fd_kernel,SLEEP_OK);
-                    log_info(logger_IO,"3");
                     break;
                  default:
                      log_info(logger_IO, "Se desconectó KERNEL");
